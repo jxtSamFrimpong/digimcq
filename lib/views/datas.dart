56c954ac7c75ba08df6c642cd9983e705729e5f1
c0l0l0s <hub4realtil@gmail.com>
2022-09-13 12:38:02 +0000

    switch-case

--- lib/views/datas.dart
+++ lib/views/datas.dart
@@ -15,17 +15,47 @@ generateCSV(uid, test_doc_id) async {
     for (var doc in querySnapshot.docs) {
       Map<String, dynamic> data = doc.data();
       //var fooValue = data['foo']; // <-- Retrieving the value.
+      const sudo = data['student_idx'];
+  late var sudoman;
+
+   try{
+    switch ( true ) { 
+   case sudo>70.0: { 
+      sudoman = 'A';
+   } break; 
+   case sudo>=60.0 && sudo <70.0: { 
+      sudoman = 'B'; 
+   } break; 
+   case sudo>=50.0 && sudo<60.0: { 
+      sudoman = 'C'; 
+   } break; 
+   case sudo>=40.0 && sudo<50.0: { 
+      sudoman = 'D'; 
+   } break; 
+   case sudo>=0.0 && sudo<40.0: { 
+      sudoman = 'F'; 
+   } break; 
+   
+   default: { 
+     sudoman = 'F';
+   } break; 
+}
+   }catch(e){
+    print(e);
+   } 
+
       Map<String, dynamic> _vals = {
         'answers': data['answers'],
         'file_id': data['file_id'],
         'score': data['got_marks'],
         'out_of': data['out_of'],
         'percentage': data['percentage'],
-        'student_idx': data['student_idx']
+        'student_idx': data['student_idx'],
+        'grade':sudoman
       };
       myList.add(_vals);
     }
-    //print(myList.length);
+    print(myList[0]);
 
     // var collection = FirebaseFirestore.instance
     //     .collection(uid)
